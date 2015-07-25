85c3b6c5996fc0b8e50642355609ecd731e00bef
Daniel <luzhanxun.daniel@gmail.com>
2015-07-24 17:42:16 -0700

    Add unit test project
    Add Hatfield.EnviroData.QualityAssurance.Test project for unit tests

---
+++ Hatfield.EnviroData.QualityAssurance.Test/DataQualityCheckingToolFactoryTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+using Hatfield.EnviroData.QualityAssurance;
+
+namespace Hatfield.EnviroData.QualityAssurance.Test
+{
+    [TestFixture]
+    public class DataQualityCheckingToolFactoryTest
+    {
+        [Test]
+        public void GenerateDataQualityChekcingToolTest()
+        {
+            var configurationToTest = new DataQualityCheckingToolConfiguration();
+
+            var factory = new DataQualityCheckingToolFactory();
+
+            Assert.Throws(typeof(NotImplementedException), () => factory.GenerateDataQualityCheckingTool(configurationToTest));
+
+
+        }
+    }
+}
