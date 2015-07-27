321c1a68d105a76ebf49946d7cca6d89e880468e
Daniel <luzhanxun.daniel@gmail.com>
2015-07-27 14:23:19 -0700

    Finish check function
    Finish check function for SampleMatrixTypeChekcingTool
    Update related unit tests

--- Source/Hatfield.EnviroData.QualityAssurance/DataQualityCheckingTool/SampleMatrixTypeCheckingTool.cs
+++ Source/Hatfield.EnviroData.QualityAssurance/DataQualityCheckingTool/SampleMatrixTypeCheckingTool.cs
@@ -11,16 +11,22 @@ namespace Hatfield.EnviroData.QualityAssurance.DataQualityCheckingTool
 {
     public class SampleMatrixTypeCheckingTool : IDataQualityCheckingTool
     {
+        private string validSampleMatrixResultFormat = "Sample Result: {0}'s sample matrix meets the expected value.";
+        private string invalidSampleMatrixResultFormat = "Sample Result: {0}'s sample matrix value is {1}, the expected value is {2}. Need data correction.";
 
         public IQualityCheckingResult Check(object data, IDataQualityCheckingRule dataQualityCheckingRule)
         {
-            if (IsDataSupport(data))
+            if (!IsDataSupport(data))
             {
-                return new QualityCheckingResult("Data is not supported by the Quality Checking Tool.", false, QualityCheckingResultLevel.Error);
+                return new QualityCheckingResult("Data is not supported by the Quality Checking Tool.", 
+                                                 false, 
+                                                 QualityCheckingResultLevel.Error);
             }
-            else if (IsDataQualityChekcingRuleSupported(dataQualityCheckingRule))
+            else if (!IsDataQualityChekcingRuleSupported(dataQualityCheckingRule))
             {
-                return new QualityCheckingResult("Data Quality Checking Rule is not supported by the Quality Checking Tool.", false, QualityCheckingResultLevel.Error);
+                return new QualityCheckingResult("Data Quality Checking Rule is not supported by the Quality Checking Tool.", 
+                                                 false, 
+                                                 QualityCheckingResultLevel.Error);
             }
             else
             {
@@ -38,18 +44,77 @@ namespace Hatfield.EnviroData.QualityAssurance.DataQualityCheckingTool
 
         public bool IsDataQualityChekcingRuleSupported(IDataQualityCheckingRule dataQualityCheckingRule)
         {
-            return dataQualityCheckingRule is StringCompareCheckingRule;
+            var isSupported = dataQualityCheckingRule is StringCompareCheckingRule;
+            return isSupported;
         }
 
         public bool IsDataSupport(object data)
         {
-            return data is Hatfield.EnviroData.Core.Action;
+            var isSupported = data is Hatfield.EnviroData.Core.Action;
+            return isSupported;
         }
 
-        private IQualityCheckingResult IsSampleMatrixTypeDataMeetQualityCheckingRule(Hatfield.EnviroData.Core.Action sampleActionData, StringCompareCheckingRule rule)
+        private IQualityCheckingResult IsSampleMatrixTypeDataMeetQualityCheckingRule(Hatfield.EnviroData.Core.Action sampleActionData, 
+                                                                                     StringCompareCheckingRule rule)
         {
-            
-            throw new NotImplementedException();
+            var resultStringBuilder = new StringBuilder();
+            var needCorrection = false;
+
+            var sampleResults = from featureAction in sampleActionData.FeatureActions
+                                from result in featureAction.Results
+                                where featureAction.SamplingFeature.SamplingFeatureTypeCV == "Site"
+                                select result;
+
+            foreach(var result in sampleResults)
+            {
+                var sampleMatrixExtension = result.ResultExtensionPropertyValues
+                                                    .Where(x => x.ExtensionProperty.PropertyName == 
+                                                           ESDATSampleCollectionConstants.ResultExtensionPropertyValueKeyMatrixType)
+                                                    .FirstOrDefault();
+
+                if (sampleMatrixExtension == null)
+                {
+                    resultStringBuilder.AppendLine("Sample Result: " + ResultToString(result) + " does not contain sample matrix data.");
+                }
+                else
+                {
+                    var stringCompareOption = rule.IsCaseSensitive ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal;
+
+                    if (string.Equals(sampleMatrixExtension.PropertyValue, rule.ExpectedValue, stringCompareOption))
+                    {
+                        resultStringBuilder.AppendLine(
+                            string.Format(validSampleMatrixResultFormat, ResultToString(result))
+                        );
+                    }
+                    else
+                    {
+                        resultStringBuilder.AppendLine(
+                            string.Format(invalidSampleMatrixResultFormat, 
+                                          ResultToString(result), 
+                                          sampleMatrixExtension.PropertyValue, 
+                                          rule.ExpectedValue)
+                        );
+                        needCorrection = true;
+                    }
+                }
+            }
+
+            var qcResult = new QualityCheckingResult(resultStringBuilder.ToString(), needCorrection, QualityCheckingResultLevel.Info);
+            return qcResult;
+        }
+
+        private string ResultToString(Result result)
+        {
+            var resultStringBuilder = new StringBuilder();
+            resultStringBuilder.Append(result.ResultUUID.ToString());
+
+            if(result.ResultDateTime.HasValue)
+            {
+                resultStringBuilder.Append(" on ");
+                resultStringBuilder.Append(result.ResultDateTime.Value.ToString("MMM-dd-yyyy"));
+            }
+
+            return resultStringBuilder.ToString();
         }
     }
 }
--- Test/Hatfield.EnviroData.QualityAssurance.Test/DataQualityCheckingTool/SampleMatrixTypeCheckingToolTest.cs
+++ Test/Hatfield.EnviroData.QualityAssurance.Test/DataQualityCheckingTool/SampleMatrixTypeCheckingToolTest.cs
@@ -17,9 +17,105 @@ namespace Hatfield.EnviroData.QualityAssurance.Test.DataQualityCheckingTool
     public class SampleMatrixTypeCheckingToolTest
     {
         [Test]
-        public void CheckTest()
-        { 
-            
+        public void DataNotSupportCheckTest()
+        {
+            var testTool = new SampleMatrixTypeCheckingTool();
+            var testQualityCheckingRule = new StringCompareCheckingRule("test", false, "test");
+
+            var dataNotSupportCheckResult = testTool.Check(1, testQualityCheckingRule);
+
+            Assert.NotNull(dataNotSupportCheckResult);
+            Assert.AreEqual("Data is not supported by the Quality Checking Tool.", dataNotSupportCheckResult.Message);
+            Assert.False(dataNotSupportCheckResult.NeedCorrection);
+            Assert.AreEqual(QualityCheckingResultLevel.Error, dataNotSupportCheckResult.Level);
+        }
+
+        [Test]
+        public void RuleNotSupportCheckTest()
+        {
+            var testTool = new SampleMatrixTypeCheckingTool();
+            var mockQualityCheckingRule = new Mock<IDataQualityCheckingRule>();
+
+            var dataNotSupportCheckResult = testTool.Check(new Hatfield.EnviroData.Core.Action(), mockQualityCheckingRule.Object);
+
+            Assert.NotNull(dataNotSupportCheckResult);
+            Assert.AreEqual("Data Quality Checking Rule is not supported by the Quality Checking Tool.", dataNotSupportCheckResult.Message);
+            Assert.False(dataNotSupportCheckResult.NeedCorrection);
+            Assert.AreEqual(QualityCheckingResultLevel.Error, dataNotSupportCheckResult.Level);
+        }
+
+        [Test]
+        public void ValidCheckTest()
+        {
+            var testActionData = new Hatfield.EnviroData.Core.Action
+            {
+                FeatureActions = new List<FeatureAction> { 
+                    new FeatureAction{
+                        SamplingFeature = new SamplingFeature{
+                            SamplingFeatureTypeCV = "Site"
+                        },
+                        Results = new List<Result>{
+                            new Result{
+                                ResultUUID = new Guid("abdc02ff-4bc6-4b8c-8578-e3c8d3620cea"),
+                                ResultDateTime = new DateTime(2015, 5, 1),
+                                ResultExtensionPropertyValues = new List<ResultExtensionPropertyValue>{
+                                    new ResultExtensionPropertyValue{
+                                        ExtensionProperty = new ExtensionProperty{
+                                            PropertyName = "Matrix Type"
+                                        },
+                                        PropertyValue = "Water"
+                                    }
+                                }
+                            },
+                            new Result{
+                                ResultUUID = new Guid("abdc02ff-4bc6-4b8c-8578-e3c8d3620cea"),                                
+                                ResultExtensionPropertyValues = new List<ResultExtensionPropertyValue>{
+                                    new ResultExtensionPropertyValue{
+                                        ExtensionProperty = new ExtensionProperty{
+                                            PropertyName = "Matrix Type"
+                                        },
+                                        PropertyValue = "water"
+                                    }
+                                }
+                            },
+                            new Result{
+                                ResultUUID = new Guid("abdc02ff-4bc6-4b8c-8578-e3c8d3620cea"),
+                                ResultDateTime = new DateTime(2015, 5, 1),
+                                ResultExtensionPropertyValues = new List<ResultExtensionPropertyValue>{
+                                    new ResultExtensionPropertyValue{
+                                        ExtensionProperty = new ExtensionProperty{
+                                            PropertyName = "Matrix Type"
+                                        },
+                                        PropertyValue = "test type"
+                                    }
+                                }
+                            },
+                            new Result{
+                                ResultUUID = new Guid("abdc02ff-4bc6-4b8c-8578-e3c8d3620cea"),
+                                ResultDateTime = new DateTime(2015, 5, 1),
+                                ResultExtensionPropertyValues = new List<ResultExtensionPropertyValue>{                                    
+                                }
+                            }
+                        
+                        }
+                    }
+                }
+            };
+
+            var testQualityCheckingRule = new StringCompareCheckingRule("Water", true, "test");
+            var testTool = new SampleMatrixTypeCheckingTool();
+
+            var qcResult = testTool.Check(testActionData, testQualityCheckingRule);
+
+            var expextedMessageBuilder = new StringBuilder();
+            expextedMessageBuilder.AppendLine("Sample Result: abdc02ff-4bc6-4b8c-8578-e3c8d3620cea on May-01-2015's sample matrix meets the expected value.");
+            expextedMessageBuilder.AppendLine("Sample Result: abdc02ff-4bc6-4b8c-8578-e3c8d3620cea's sample matrix meets the expected value.");
+            expextedMessageBuilder.AppendLine("Sample Result: abdc02ff-4bc6-4b8c-8578-e3c8d3620cea on May-01-2015's sample matrix value is test type, the expected value is Water. Need data correction.");
+            expextedMessageBuilder.AppendLine("Sample Result: abdc02ff-4bc6-4b8c-8578-e3c8d3620cea on May-01-2015 does not contain sample matrix data.");
+
+            Assert.AreEqual(expextedMessageBuilder.ToString(), qcResult.Message);
+            Assert.True(qcResult.NeedCorrection);
+            Assert.AreEqual(QualityCheckingResultLevel.Info, qcResult.Level);
         }
 
         [Test]
